616ec15a19199f5307540fd249d22f3a48deb132
koechronix <koechronix@yahoo.com>
2024-11-24 09:33:52 +0300

    Initial commit

---
+++ backend/main.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def is_alive():
+    return jsonify('live')
+
+
+@app.route('/api/msg/<string:msg>', methods=['POST'])
+def msg_post_api(msg):
+    print(f"msg_post_api with message: {msg}")
+    # TODO: store msg in DB and return identifier
+    return jsonify({'msg_id': msg_id})
+
+
+@app.route('/api/msg/<int:msg_id>', methods=['GET'])
+def msg_get_api(msg_id):
+    print(f"msg_get_api > msg_id = {msg_id}")
+    # TODO: get msg from DB and return it
+    return jsonify({'msg': msg})
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1")
